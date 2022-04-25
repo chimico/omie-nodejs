f213b2f9114ef1848dd696c6f4d61ac2b0e40f79
Felipe de Albuquerque <felipe.albuquerque@somagrupo.com.br>
2022-04-25 17:21:47 -0300

    Add second test and refactor code

--- tests/customer.test.js
+++ tests/customer.test.js
@@ -1,26 +1,56 @@
-const omie = require('../src/omie')({ key: 'FAKEKEY', secret: 'FAKESECRET'});
 const nock = require('nock');
 const chai = require('chai');
 
+const OMIE_KEY = 'FAKEKEY';
+const OMIE_SECRET = 'FAKESECRET';
+
+const omie = require('../src/omie')({ key: OMIE_KEY, secret: OMIE_SECRET });
+
 chai.should();
 
-nock('https://app.omie.com.br/api/v1')
-  .post('/geral/clientes/', {
+const OMIE_URL = 'https://app.omie.com.br/api/v1';
+const CUSTOMER_URL = '/geral/clientes/';
+
+nock(OMIE_URL)
+  .post(CUSTOMER_URL, {
     call: 'ConsultarCliente',
-    app_key: 'FAKEKEY',
-    app_secret: 'FAKESECRET',
-    param: [{ codigo_cliente_omie: 1 }]
+    app_key: OMIE_KEY,
+    app_secret: OMIE_SECRET,
+    param: [{
+      codigo_cliente_omie: 1,
+    }],
   })
   .reply(200, {
     codigo_cliente_omie: 1,
   });
 
+nock(OMIE_URL)
+  .post(CUSTOMER_URL, {
+    call: 'ConsultarCliente',
+    app_key: OMIE_KEY,
+    app_secret: OMIE_SECRET,
+    param: [{
+      codigo_cliente_integracao: '2',
+    }],
+  })
+  .reply(200, {
+    codigo_cliente_integracao: '2',
+  });
+
 describe('Customer Resource', () => {
   describe('retrieve', () => {
     it('Sends the correct request with id', async () => {
-      const data = await omie.general.customers.retrieve(codigo_cliente_omie);
+      const data = await omie.general.customers.retrieve(1);
 
       data.should.have.property('codigo_cliente_omie').with.equal(1);
+    });
+
+    it ('Sends the correct request with integration code', async() => {
+      const data = await omie.general.customers.retrieve({
+        integrationCode: '2',
+      });
+
+      data.should.have.property('codigo_cliente_integracao').with.equal('2');
     });
   });
 });
