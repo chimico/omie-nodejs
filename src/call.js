165ea59e9f39b808732a921215a48f7679b6511c
Felipe de Albuquerque <felipe.albuquerque@somagrupo.com.br>
2022-06-13 15:25:08 -0300

    Remove unnecessary file

--- src/call.js
+++
@@ -1,8 +0,0 @@
-const omie = require('./omie');
-
-async function useOmie() {
-  const objectCall = await omie('teste', 12).general.customers.retrieve({integrationCod: 2});
-  console.log(objectCall);
-}
-
-useOmie();
