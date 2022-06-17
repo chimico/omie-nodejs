a7e798c5b8677bdbbe18da98e3f74036fd699c03
blueishot <fabricioteixeirahungria@gmail.com>
2022-06-17 09:30:46 -0300

    Param validation Fixed

--- src/omie.js
+++ src/omie.js
@@ -1,17 +1,78 @@
-const OmieRequestBody = require('./OmieRequestBody');
-const customers = require('./customers');
+const nodeFetch = require('node-fetch');
+const paramValidation = require('./validation');
+const generateRequestBody = require('./request');
 
-const Omie = ({ key, secret }) => {
-  if (key === undefined || secret === undefined) {
-    throw new Error('Key and Secret must be provided');
+const BASE_URL = 'https://app.omie.com.br/api/v1';
+
+const methodList = {
+  ConsultarCliente: ['codigo_cliente_omie', 'codigo_cliente_integracao'],
+  IncluirCliente: [
+    'codigo_cliente_integracao',
+    'email',
+    'razao_social',
+    'nome_fantasia',
+  ],
+  AlternarCliente: [
+    'codigo_cliente_integracao',
+    'email',
+    'razao_social',
+    'nome_fantasia',
+  ],
+  AssociarCodIntCliente: ['codigo_cliente_omie', 'codigo_cliente_integracao'],
+  UpsertCliente: [
+    'codigo_cliente_integracao',
+    'email',
+    'razao_social',
+    'nome_fantasia',
+  ],
+  UpsertClienteCpfCnpj: ['cnpj_cpf', 'email', 'razao_social', 'nome_fantasia'],
+  ListaClientes: ['pagina', 'registros_por_pagina', 'apenas_importado_api'],
+};
+
+const Omie = ({key, secret, method}) => {
+
+  paramValidation(key, secret);
+
+  for (const methodCheck in methodList) {
+    if (methodList.hasOwnProperty(method)) {
+      const pickMethod = methodList[method];
+      return pickMethod;
+    } else {
+      return 'Incorrect Method';
+    }
   }
-
-  // Create the single instance
-  new OmieRequestBody(key, secret);
 
   return {
     general: {
-      customers,
+      customers: {
+        retrieve: async (params) => {
+          const paramsProcessed = paramValidation(params);
+          if (paramsProcessed == 'inválido') {
+            return 'inválido';
+          }
+          const bodyParams = {};
+          if (Number.isFinite(params)) {
+            bodyParams.codigo_cliente_omie = params;
+          } else {
+            bodyParams.codigo_cliente_integracao = params.integrationCode;
+          }
+
+          const URL = `${BASE_URL}/geral/clientes/`;
+          const requestBody = generateRequestBody(
+            key,
+            secret,
+            'ConsultarCliente',
+            bodyParams
+          );
+
+          const response = await nodeFetch(URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: requestBody,
+          });
+          return response.json();
+        },
+      },
     },
   };
 };
--- src/validation.js
+++ src/validation.js
@@ -1,10 +1,20 @@
 const isPlainObject = require('lodash.isplainobject');
-
 const has = require('./utils');
 const WrongParamError = require('./errors/WrongParamError');
 
 // FIXME: Essa função só faz a validação do método retrieve do cliente
-function paramValidation(params) {
+function paramValidation(key, secret, params) {
+  if (key === undefined || secret === undefined) {
+    throw new WrongParamError({
+      param: [key, secret],
+      detail: 'key or secret is empty',
+      statusCode: 400,
+      message: 'Please, check the key and secret',
+    });
+  } else {
+    return key && secret;
+  }
+
   if (isPlainObject(params)) {
     if (has(params, 'integrationCode')) {
       return { codigo_cliente_integracao: params.integrationCode };
